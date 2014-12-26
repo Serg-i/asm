4eda0706e25a4d5229a3913b55275ce09271c3e7
Serg-i <serg.mayakou@gmail.com>
2014-12-27 01:16:31 +0300

    multiplying integer numbers

--- bcdNumbers.asm
+++ bcdNumbers.asm
@@ -1,14 +1,15 @@
 .model small
 .386
-.stack
+.stack 200h
 .data
 buff db 255,?,255 dup(?); buffer for string
 course db 0,5,7,3,1; default course
 course_l db 5
-usd db 20 dup(?)
+usd db 20 dup(0)
 usd_l db 0 
-belki db 40 dup(?)
+belki db 40 dup(0)
 belki_l db 0
+tmp_numb db 0
 filePath db "file.txt",0
 fileDesc dw ?
 menuMess db 10,13,"write ",10,13," 1 - from BYR in USD ",10,13," 2 - from USD in BYR ",10,13," 3 - set course ",10,13,"$"
@@ -20,8 +21,8 @@ helpMess db 10,13,"press esc to exit in menu","$"
 endLoopMess db 10,13,"end of loop",10,13,"$"
 endMessage db 10,13,"Click to close",10,13,"$"
 file_error_mess db 10,13,"error: program can't open/create a file",10,13,"$"
-a db 9,9,9,9,1
-b db 8,1,4,5,6,7,8
+a db 0,0,5,7,3,1
+b db 0,5,2,1,4
 result db 50 dup(0)
 result_l db 0
 .code                                      
@@ -41,9 +42,9 @@ menu:
 	;test sum
 	xor dx,dx
 	lea si,b
-	mov dh,7
+	mov dh,5
 	lea di,a
-	mov dl,5
+	mov dl,6
 	call sum_bcd
 	lea si,result
 	mov cl,result_l
@@ -64,10 +65,14 @@ first:
 	mov [usd_l],al
 	lea dx,get_byr_mess
 	call write_on_screen
+	lea di,course
+	mov dl,course_l
+	lea si,usd
+	mov dh,usd_l
 	call mult_bcd
-	mov cl,belki_l
-	lea si,belki
-	call write_number_to_buf	
+	mov cl,result_l
+	lea si,result
+	call write_number_to_buf
 	lea dx,buff
 	add dx,2
 	call write_on_screen
@@ -112,6 +117,19 @@ proc clear_buff
 	loop clear_loop
 	ret
 endp clear_buff
+proc clear_bcd
+	mov cl,belki_l
+	cmp cl,0
+	je end_clear_bcd 
+	xor si,si
+	clear_bcd_loop:
+		mov belki[si],0
+		inc si
+	loop clear_bcd_loop
+	mov belki_l,0
+	end_clear_bcd:
+	ret 
+endp clear_bcd
 ; read from buff to SI  
 ;	IN:		SI- addres of BCD 
 ;	OUT:	Al - length of bcd
@@ -165,34 +183,75 @@ proc write_course_on_screen
 	call write_on_screen 
 	ret
 endp write_course_on_screen
+;IN:	DI - effective address of first number
+;		SI - effective addrest of second number
+;	 	dl - length of first number 
+;		dh - length of second number
 ; multiply bcd numbers (tmp) course*usd
 ;todo:	1)clear result before perform multiplying
 ;		2)more than one number in usd:)
 ;		3)round to given accuracy
+;		4)use stack
 proc mult_bcd
+	push di
+	xor ax,ax;start at first digit
+	mov al,dh
+	add si,ax
+	push si
+	push dx 
+	mul_general_loop:
+	call clear_bcd
+	mov tmp_numb,0
 	xor ax,ax
-	xor bx,bx
 	xor dx,dx
+	xor bx,bx 
 	xor cx,cx
-	xor di,di;parameter!
-	xor si,si;parameter!
-	lea si,usd
-	lea di,course
-	mov cl,course_l;parameter!
+	xor di,di
+	xor si,si
+	pop dx; dl - first_l, dh - second_l
+	pop si;second
+	pop di;first 
+	cmp dh,0
+	jna end_mult_bcd
+	dec si
+	mov al,[si]
+	mov tmp_numb,al
+	dec dh
+	cmp dh,0
+	push di
+	jna adding_zero_not_needed
+	mov cl,dh
+	xor bx,bx
+	add_zeroes_at_end:
+		mov belki[bx],0
+		inc bx
+	loop add_zeroes_at_end
+	sub di,bx
+	adding_zero_not_needed:
+	push si
+	push dx; save length of numbers
+	mov cl,dl
+	xor dx,dx
 	mul_loop:
-		mov al,[di]
-		mul usd
+		mov al,[di+bx]
+		mul tmp_numb
 		aam
 		adc al,dl
 		aaa
 		mov dl,ah
 		mov belki[bx],al
-		inc di
 		inc bx
 	loop mul_loop
 	mov belki[bx],dl
-	mov belki_l,bl;out parameter! or rename to result :)
-ret
+	mov belki_l,bl 
+	lea di,belki
+	mov dl,bl
+	lea si,result
+	mov dh,result_l
+	call sum_bcd
+	jmp mul_general_loop
+	end_mult_bcd:
+	ret
 endp mult_bcd
 ;IN:	DI - effective address of first number
 ;		SI - effective addrest of second number
@@ -204,6 +263,25 @@ proc sum_bcd
 	xor cx,cx
 	xor ax,ax
 	xor bx,bx
+	cmp dx,0
+	je end_sum_bcd
+	cmp dl,0
+	jne first_not_zero
+	mov cl,dh
+	mov di,si
+	jmp set_numb
+	first_not_zero:
+	cmp dh,0
+	jne both_not_zero
+	mov cl,dl
+	set_numb:
+		mov al,[di]
+		mov result[bx],al
+		inc di
+		inc bx
+	loop set_numb
+	jmp end_sum_bcd
+both_not_zero:
 	cmp dl,dh
 	jne set_min
 	mov cl,dl
@@ -228,7 +306,7 @@ proc sum_bcd
 	loop sum_loop
 	adc result[bx],0
 	cmp dl,dh
-	je fin_sum
+	je end_sum_bcd
 	clc
 	jg greater_num
 	sub dh,dl
@@ -240,16 +318,18 @@ proc sum_bcd
 	mov cl,dl
 	correct_num:
 		mov al,result[bx]
-		adc al,[di]
+		adc al,0
 		aaa
 		mov result[bx],al
 		inc bx
 		inc di
 	loop correct_num
 	adc result[bx],0
-	fin_sum:
+	end_sum_bcd:
 	mov result_l,bl
 	ret
+	xor di,di 
+	xor si,si
 endp sum_bcd 
 proc open_file
 	xor ax,ax 
